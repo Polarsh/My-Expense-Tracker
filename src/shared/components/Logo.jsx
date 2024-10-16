b80dad4505811e849790855d2dc8ba5ff6bb6f9e
Polar <dybsm98@outlook.es>
2024-10-15 21:49:49 -0500

    Cambio para centrar el icono

--- src/shared/components/Logo.jsx
+++ src/shared/components/Logo.jsx
@@ -5,7 +5,11 @@ export default function LogoComponent({ size = 'h-8', clickable = false }) {
   const logoElement = (
     <div>
       <span className='sr-only'>{COMPANY_NAME}</span>
-      <img className={`w-auto ${size}`} src={COMPANY_LOGO} alt={COMPANY_NAME} />
+      <img
+        className={`w-auto mx-auto ${size}`}
+        src={COMPANY_LOGO}
+        alt={COMPANY_NAME}
+      />
     </div>
   )
 
